71df46dbffc3a947505e6b078dfd346e14ad334d
Mo Sorkhpar <Sorkhpar@outlook.com>
2023-08-19 10:50:58 -0400

    Catch exceptions

--- utils/confusion_matrix_builder.py
+++ utils/confusion_matrix_builder.py
@@ -1,29 +1,23 @@
 import csv
-import os.path
 
-import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, accuracy_score
 
 
 def build_confusion_matrix(result_csv_file_path, output_path):
-    confusion_matrix = np.zeros((10, 10), dtype=int)
 
+    actual = []
+    predicted = []
     with open(result_csv_file_path, 'r') as file:
         reader = csv.reader(file)
         next(reader, None)
-
         for row in reader:
-            actual = int(row[0].split("_")[0])
-            detected = int(row[1].split("_")[0])
-            confusion_matrix[actual][detected] += 1
+            actual.append(int(row[0].split("_")[0]))
+            predicted.append(int(row[1].split("_")[0]))
 
-    row_sums = confusion_matrix.sum(axis=1, keepdims=True)
-    normalized_confusion_matrix = confusion_matrix / row_sums * 100
-
-    plt.figure(figsize=(10, 8))
-    sns.heatmap(normalized_confusion_matrix, annot=True, cmap='YlGnBu', fmt='.2f', cbar_kws={'label': 'Percentage (%)'})
-    plt.xlabel('Predicted')
-    plt.ylabel('Actual')
-    plt.title('Confusion Matrix')
-    plt.savefig(os.path.join(output_path, 'confusion_matrix.png'), dpi=300, bbox_inches='tight')
+    matrix = confusion_matrix(actual, predicted)
+    cm_display = ConfusionMatrixDisplay(confusion_matrix=matrix)
+    accuracy = accuracy_score(actual, predicted)
+    print("Accuracy:", accuracy)
+    cm_display.plot()
+    plt.savefig(output_path + "/confusion_matrix.png")
